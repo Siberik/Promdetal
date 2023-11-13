a048043c4579b9602933b8a45a98dd76eaaae93c
GoldenDragon <you@example.com>
2023-11-13 21:05:34 +0500

    Проблема со шрифтами

--- МОЙ ПРОЕКТ/index.php
+++ МОЙ ПРОЕКТ/index.php
@@ -19,9 +19,32 @@
 <body class="page__body">
 	
 	<div class="site-container">
-	<?php
-		include 'menu.php';
-		?>
+	
+		<header class="header">
+			<div class="container">
+			<div class="header__top"><a href="index.html" class="logo header__logo"><img src="images/logo.png" alt="Логотип" width="138" height="38"></a>
+					<div class="header__contacts">
+						<div class="header__contact"><span class="header__caption">По вопросам и предложениям</span> <a href="mailto:info@promdetal-ek.ru" class="header__link">info@promdetal-ek.ru </a></div>
+						<div class="header__contact"><span class="header__caption">Для консультаций</span> <a href="tel:+73432169830 " class="header__link">+7 (343) 216-98-30</a></div>
+					</div><button class="burger" aria-label="Открыть меню" aria-expanded="false" data-burger=""><span class="burger__line"></span></button></div>
+				<div class="header__bottom" data-menu="">
+					<nav class="nav" title="Главная навигация">
+						<ul class="list-reset nav__list">
+							<li class="nav__item"><a href="#" class="nav__link">Главная</a></li>
+							<li class="nav__item"><a href="#" class="nav__link">О предприятии</a></li>
+							<li class="nav__item"><a href="#" class="nav__link">Новости</a></li>
+							<li class="nav__item"><a href="#" class="nav__link">Продукция</a></li>
+							<li class="nav__item"><a href="#" class="nav__link">Услуги</a></li>
+							<li class="nav__item"><a href="#" class="nav__link">Поддержка</a></li>
+							<li class="nav__item"><a href="#" class="nav__link">География поставок</a></li>
+							<li class="nav__item"><a href="#" class="nav__link">Контакты</a></li>
+						</ul>
+					</nav><button class="btn-reset header__btn"><svg><use xlink:href="images/sprite.svg#phone"></use></svg> Перезвоните мне</button>
+					<div class="header__contact header__contact--mobile"><span class="header__caption">По вопросам и предложениям</span> <a href="mailto:pionerrr@sila.ru" class="header__link">promdetail@sila.ru</a></div>
+					<div class="header__contact header__contact--mobile"><span class="header__caption">Для консультаций</span> <a href="tel:+74954562421" class="header__link">+7 (495) 456-24-21</a></div>
+				</div>
+			</div>
+		</header>
 		<main class="main">
 			<section class="hero section-offset">
 				<div class="container">
