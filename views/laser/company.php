1e38b9bdf663b5bf500ff564db3c6c19bd5134ad
GoldenDragon <you@example.com>
2023-11-20 20:44:21 +0500

    Re

--- views/laser/company.php
+++ views/laser/company.php
@@ -3,6 +3,7 @@
 
 <head>
     <title>О предприятии</title>
+
     <style>
 
         .container_segment {
@@ -102,18 +103,29 @@
         }
 
         
+        .title_us {
+    margin: var(--lg) auto 20px;
+    font-weight: 700;
+    font-size: 48px;
+    line-height: 120%;
+    color: var(--dark);
+    text-align: center;
+}
+
 
 
     </style>
 </head>
 <div class="container">
 <div class="hero__top grid">
-    <h2 class="section-title about__title">Коротко о  предприятии</h2>
-</div><img src="/images/hero.jpg" srcset="/images/hero_2x.jpg 2x" alt="Промдеталь - предприятие лазерной резки" width="1180" height="440" class="hero__image"></div>
+
+</div>
+<h2 class="title_us">Коротко о  предприятии</h2>
+<img src="/images/hero.jpg" srcset="/images/hero_2x.jpg 2x" alt="Промдеталь - предприятие лазерной резки" width="1180" height="440" class="hero__image"></div>
 
 <section class="about section-offset">
     <div class="container">
-
+    
         <div class="about__text grid">
             <p class="about__descr">Компания «Промдеталь» предоставляет услуги по лазерной резке и промышленной гравировке изделий из металла и других материалов с применением лазерных технологий. Коллектив компании – профессионалы с большим опытом работы в своей отрасли, которые знают все тонкости и специфику оборудования, что позволяет повысить качество и производительность работы в области лазерной резки.</p>
             <p class="about__descr">Спектр предоставляемых услуг нашей компании довольно обширен: от стандартной лазерной резки до выполнения сложнейших заказов по индивидуально составленным чертежам, изготовления изделий и конструкций для применения в наружной рекламе, оформления интерьера, а так же эксклюзивных нестандартных изделий и изделий производственного назначения.</p>
@@ -122,11 +134,11 @@
         <div class="container_segment">
 
             <div class="row">
-                <h2 class="section-title about__title">Наши сосалы</h2>
+                <h2 class="title_us">Наши сосалы</h2>
                 <div class="col-md-4 col-sm-6">
                     <div class="our-team">
                         <div class="team-img">
-                            <img src="https://images.pexels.com/photos/247206/pexels-photo-247206.jpeg?w=940&h=650&auto=compress&cs=tinysrgb">
+                            <img src="https://n1s1.hsmedia.ru/aa/e0/9d/aae09d118ee5c3d38adaa4b41fe64ebf/617x540_1_031579db22404e4c384057a436df1770@986x863_0xac120004_18784161701688063759.jpeg">
                         </div>
                         <div class="team-content">
                             <h3 class="name">Пидораска Олеговна</h3>
@@ -139,7 +151,7 @@
                 <div class="col-md-4 col-sm-6">
                     <div class="our-team">
                         <div class="team-img">
-                            <img src="https://images.pexels.com/photos/247293/pexels-photo-247293.jpeg?w=940&h=650&auto=compress&cs=tinysrgb">
+                            <img src="https://masterpiecer-images.s3.yandex.net/a0ee3eb16abb11ee8675b646b2a0ffc1:upscaled">
                         </div>
                         <div class="team-content">
                             <h3 class="name">Сучка Альбертовна</h3>
@@ -152,7 +164,7 @@
                 <div class="col-md-4 col-sm-6">
                     <div class="our-team">
                         <div class="team-img">
-                            <img src="https://images.pexels.com/photos/247203/pexels-photo-247203.jpeg?w=940&h=650&auto=compress&cs=tinysrgb">
+                            <img src="https://intrigue.dating/wp-content/cache/thumb/11/442463fbd700f11_350x0.jpg">
                         </div>
                         <div class="team-content">
                             <h3 class="name">Стелла Еблановна</h3>
